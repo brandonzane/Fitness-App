cb3e98ccf15e61220c2b1a857e03276e2bd1dac8
brandonzane <bmushori@gmail.com>
2023-12-26 16:11:33 +0200

    Added frist screens

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import WorkoutScreens from "./src/Screens/WorkoutScreen";
+import TimerScreen from "./src/Screens/TimerScreen";
+import CalculationScreen from "./src/Screens/CalculationScreen";
+import { MaterialCommunityIcons, Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  function TabNavigator() {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarHideOnKeyboard: true,
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+            switch (route.name) {
+              case "Workout":
+                iconName = "dumbbell";
+                return (
+                  <MaterialCommunityIcons
+                    name={iconName}
+                    size={size}
+                    color={color}
+                  />
+                );
+              case "Timer":
+                iconName = "timer-outline";
+                return <Ionicons name={iconName} size={size} color={color} />;
+
+              case "Calculation":
+                iconName = "calculator-outline";
+                return <Ionicons name={iconName} size={size} color={color} />;
+            }
+          },
+          tabBarShowLabel: false,
+          headerShown: false,
+          tabBarStyle: {
+            backgroundColor: "black",
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingVertical: 5,
+            height: 70,
+          },
+          tabBarActiveTintColor: "#12E195",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen name="Workout" component={WorkoutScreens} />
+        <Tab.Screen name="Timer" component={TimerScreen} />
+        <Tab.Screen name="Calculation" component={CalculationScreen} />
+      </Tab.Navigator>
+    );
+  }
   return (
-    <View className="bg-red-400">
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="TabNav"
+          component={TabNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
